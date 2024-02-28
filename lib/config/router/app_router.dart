a26fd1c4da496b1552b9c05b24dbd87eac59ec93
Freddy Panchi <freddy@MacBook-Air-de-Freddy.local>
2024-02-28 17:14:41 -0500

    complete buttons screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,7 +2,16 @@ import 'package:go_router/go_router.dart';
 import 'package:widgets_app/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
-  GoRoute(path: '/buttons', builder: (context, state) => const ButtonsScreen()),
-  GoRoute(path: '/cards', builder: (context, state) => const CardsScreen()),
+  GoRoute(
+      name: HomeScreen.name,
+      path: '/',
+      builder: (context, state) => const HomeScreen()),
+  GoRoute(
+      name: ButtonsScreen.name,
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen()),
+  GoRoute(
+      name: CardsScreen.name,
+      path: '/cards',
+      builder: (context, state) => const CardsScreen()),
 ]);
--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
+  static const String name = 'buttons_screen';
   const ButtonsScreen({super.key});
 
   @override
@@ -9,7 +11,96 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(
+                onPressed: null, child: Text('Elevated disabled')),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: const Text('Elevated Icon'),
+              icon: const Icon(Icons.alarm_add_rounded),
+            ),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: () {},
+              label: const Text('Filled icon'),
+              icon: const Icon(Icons.accessibility_outlined),
+            ),
+            OutlinedButton(onPressed: () {}, child: const Text('Outline')),
+            OutlinedButton.icon(
+              onPressed: () {},
+              label: const Text('Outline'),
+              icon: const Icon(Icons.snapchat_outlined),
+            ),
+            TextButton(onPressed: () {}, child: const Text('Text')),
+            TextButton.icon(
+              onPressed: () {},
+              label: const Text('Text'),
+              icon: const Icon(Icons.terminal),
+            ),
+            const CustomButton(),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.stacked_bar_chart)),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.stacked_bar_chart),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(colors.primary),
+                  iconColor: const MaterialStatePropertyAll(Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding:
+                EdgeInsetsDirectional.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'Custom',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
